3706e109eb3d4edbd44912ae0b931d6c9e222879
Thi Linh Chi le <linhchi1152004@gmail.com>
2024-04-27 23:24:40 +0200

    SecondLesson.py updated

--- Lesson/SecondLesson.py
+++ Lesson/SecondLesson.py
@@ -40,13 +40,43 @@
 # for i in range(10):
 #     print("{}: ".format(i), i * "+")
 
-names = ['John', 'Doe', 'Jane']
-print(names[0])
-print(names[-1])
-names[2] = 'Julia'
-print(names)
-print(names[0:2]) #excluding the element of index 2
+# names = ['John', 'Doe', 'Jane']
+# print(names[0])
+# print(names[-1])
+# names[2] = 'Julia'
+# print(names)
+# print(names[0:2]) #excluding the element of index 2
 # -1 represents the last element of the list
+#
+# # list methods
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# numbers.insert(0,1)
+# numbers.remove(3)
+# print(numbers)
+# print(1 in numbers)
+# print(len(numbers))
+# for i in numbers:
+#     # just line for(int i: numbers) in java
+#     print(i)
+# # there isnt any print and println in python, print will automatically enter when executed
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
+#
+# # use range function to generate a sequence of numbers
+# range(5)
+# # from 0 to 4
+# for numbers in range(5, 10, 2):
+#     print(numbers)
+
+# tuple is like list but immutable
+list = (1, 2, 3, 4, 5)
+print(list.index(3, 0, len(list)))
+print(list.__add__((6, 7, 8)))
+print(list.__getitem__(0))
 
 
 
